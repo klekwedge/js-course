7c46d99b3a702301e57efac107f6bef961ad7ac3
klekkwedge <antonov.andrey.10101@gmail.com>
2022-03-09 19:06:49 +0300

    feat: do second homework

--- script.js
+++ script.js
@@ -6,15 +6,34 @@ const personalMovieDB = {
   movies: {},
   actors: {},
   genres: [],
-  privat: false
+  privat: false,
 };
 
-const lastFilm = prompt("Один из последних просмотренных фильмов?", "");
-const lastFilmRating = prompt("На сколько оцените его?", "");
+for (let i = 0; i < 2; i++) {
+  const lastFilm = prompt("Один из последних просмотренных фильмов?", "");
+  const lastFilmRating = prompt("На сколько оцените его?", "");
+  if (
+    lastFilm.length === "" ||
+    lastFilm.length > 50 ||
+    lastFilmRating.length === "" ||
+    lastFilmRating.length > 50 ||
+    lastFilm === null ||
+    lastFilmRating === null
+  ) {
+    i--;
+    continue;
+  }
+  personalMovieDB.movies[lastFilm] = lastFilmRating;
+}
 
-const yetlastFilm = prompt("Один из последних просмотренных фильмов?", "");
-const yetlastFilmRating = prompt("На сколько оцените его?", "");
-
-personalMovieDB.movies[lastFilm] = lastFilmRating;
-personalMovieDB.movies[yetlastFilm] = yetlastFilmRating;
 console.log(personalMovieDB);
+
+if (personalMovieDB.count < 10) {
+  console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+  console.log("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+  console.log("Вы киноман");
+} else {
+  console.log("Произошла ошибка");
+}
