57fff35b0f7999091582f5cd3a1bcd1ee91c96fa
klekkwedge <antonov.andrey.10101@gmail.com>
2022-03-12 14:23:56 +0300

    feat: add 5 lesson

---
+++ 1. JS basics/5 Interpolation/script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const category = "cars";
+console.log(`https://someurl.com/` + category + "/" + "4");
+console.log(`https://someurl.com/${category}/4`);
+
+const user = 'Mike';
+console.log(`Hello ${user}!`);
