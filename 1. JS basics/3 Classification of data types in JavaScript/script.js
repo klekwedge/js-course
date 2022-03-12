d865034597ed0ff28acfd3c139eed20177999d78
klekkwedge <antonov.andrey.10101@gmail.com>
2022-03-12 14:51:51 +0300

    feat: add 3 lesson

---
+++ 1. JS basics/3 Classification of data types in JavaScript/script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+let number = 4.6;
+
+console.log(4 / 0);
+console.log(-4 / 0);
+
+console.log("str" * 9);
+console.log(something); //null
+
+const bool = false;
+
+let und;
+console.log(und); //undefined
+
+const obj = {
+  name: "Alex",
+  age: 23,
+  isMarried: false,
+};
+
+console.log(obj);
+console.log(obj.name);
+console.log(obj["name"]);
+
+let arr = [4, 3, 7, 1, [], {}, "example", 2];
+console.log(arr[1]);
