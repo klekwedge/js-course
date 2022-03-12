9bae656df7cff1b745d7c7cf54360affc6821fa3
klekkwedge <antonov.andrey.10101@gmail.com>
2022-03-12 14:56:52 +0300

    feat: add 2 lesson

---
+++ 1. JS basics/2 Variables and strict mode/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+  a: 50,
+};
+
+obj.a = 15;
+console.log(obj.a);
+
+
+let namePerson = 'Alex';
+namePerson = 'John';
+console.log(namePerson);
