c29d9fd4956f8532f45bd78892df18852249e40f
klekkwedge <antonov.andrey.10101@gmail.com>
2022-03-20 12:55:00 +0300

    feat: add 12 lesson

---
+++ 2. JS in work/12 Classes (ES6)/script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+class Rectangle {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+
+  calcArea() {
+    return this.height * this.width;
+  }
+}
+
+class ColoredRectangleWithText extends Rectangle{
+  constructor(height, width, text, bgColor) {
+    super(height, width);
+    this.text = text;
+    this.bgColor = bgColor;
+  }
+
+  showMyProps() {
+    console.log(`Text: ${this.text}, color: ${this.bgColor}`);
+  }
+}
+
+// const square = new Rectangle(15, 10);
+// const long = new Rectangle(20, 100);
+// console.log(square.calcArea());
+// console.log(long.calcArea());
+
+const div = new ColoredRectangleWithText(25, 10, "Hello", "black");
+div.showMyProps();
+console.log(div.calcArea());
