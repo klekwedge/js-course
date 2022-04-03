5190c56692fbeb6f396d3b9abc660c4af4156af9
klekkwedge <antonov.andrey.10101@gmail.com>
2022-04-03 16:31:50 +0300

    feat: add 15 lesson

---
+++ 3. JS Advanced/15 How to save data without a database/script.js
@@ -0,0 +1,43 @@
+"use strict";
+
+// localStorage.setItem("number", 5);
+
+// localStorage.removeItem("number");
+// localStorage.clear();
+
+// console.log(localStorage.getItem("number"));
+
+const checkbox = document.querySelector("#checkbox"),
+  form = document.querySelector("form"),
+  change = document.querySelector("#color");
+
+if(localStorage.getItem('isChecked')){
+    checkbox.checked = true;
+}
+
+if(localStorage.getItem('bg') === 'changed'){
+    form.style.backgroundColor = 'red';
+}
+
+checkbox.addEventListener('change',()=>{
+    localStorage.setItem('isChecked', true);
+}); 
+
+change.addEventListener('click',()=>{
+    if(localStorage.getItem('bg') === 'changed'){
+        localStorage.removeItem('bg');
+        form.style.backgroundColor = 'white';
+    }
+    else{
+        localStorage.setItem('bg', 'changed');
+        form.style.backgroundColor = 'red';
+    }
+}); 
+
+const person = {
+    name: 'Alex',
+    age: 25
+};
+const serializedPesron = JSON.stringify(person);
+localStorage.setItem('alex', serializedPesron);
+console.log(JSON.parse(localStorage.getItem('alex')));
