35a3ae41d81f7452bafebaeaf7fae0588f6da676
klekkwedge <antonov.andrey.10101@gmail.com>
2022-04-10 15:56:26 +0300

    feat: add 20 lesson

---
+++ 3. JS Advanced/20 Encapsulation/script.js
@@ -0,0 +1,53 @@
+"use strict";
+
+// function User(name, age) {
+//   this.name = name;
+//   let userAge = age;
+
+//   this.say = function () {
+//     console.log(`Username: ${this.name}, age: ${userAge}`);
+//   };
+
+//   this.getAge = function () {
+//     return userAge;
+//   };
+
+//   this.setAge = function (age) {
+//     if (typeof age === "number" && age > 0 && age < 110) {
+//       userAge = age;
+//     } else {
+//       console.log("Invalid value");
+//     }
+//   };
+// }
+
+class User {
+  constructor(name, age) {
+    this.name = name;
+    this._age = age;
+  }
+
+  say() {
+    console.log(`Username: ${this.name}, age: ${this._age}`);
+  }
+
+  get age() {
+    return this._age;
+  }
+
+  set age(age) {
+    if (typeof age === "number" && age > 0 && age < 110) {
+      this._age = age;
+    } else {
+      console.log("Invalid value");
+    }
+  }
+}
+
+const ivan = new User("Ivan", 27);
+
+console.log(ivan.age);
+ivan.age = 99;
+console.log(ivan.age);
+ivan.say();
+
