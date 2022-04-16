1d1837115547ae5cca6aa12ca7a47e291cfc0a91
klekkwedge <antonov.andrey.10101@gmail.com>
2022-04-16 12:03:58 +0300

    feat: add 26 lesson

---
+++ 3. JS Advanced/26 Errors. How to avoid breaking your code/js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+try {
+  console.log("Normal");
+  console.log(a);
+  console.log("Result");
+} catch (error) {
+  console.log(error);
+  console.log(error.name);
+  console.log(error.message);
+  console.log(error.stack);
+}finally{
+  console.log('Finally');
+}
+
+console.log("Still normal");
