a8d7fee2f691d8ec9eb94f49c8604cb82beb369d
klekkwedge <antonov.andrey.10101@gmail.com>
2022-04-17 11:33:41 +0300

    feat: add 29 lesson

---
+++ 3. JS Advanced/29 jquery library/script.js
@@ -0,0 +1,24 @@
+import $ from "jquery";
+
+// const btn = $("#btn");
+// console.log(btn);
+
+$(document).ready(function () {
+  $(".list-item:first").hover(function () {
+    $(this).toggleClass("active");
+  });
+
+  $(".list-item:eq(2)").on("click", () => {
+    $(".image:even").fadeToggle();
+  });
+
+  $(".list-item:eq(4)").on("click", () => {
+    $(".image:odd").animate(
+      {
+        opacity: "toggle",
+        height: "toggle",
+      },
+      2000
+    );
+  });
+});
