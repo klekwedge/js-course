954fa040613ab8292dcaa5c2cc72924f73c6b035
klekkwedge <antonov.andrey.10101@gmail.com>
2022-05-06 20:27:34 +0300

    feat: add 10 lesson

---
+++ 4. React library. A basic level of/10 Practice of properties on the project/my-app/src/components/app/app.js
@@ -0,0 +1,40 @@
+import AppInfo from "../app-info/app-info";
+import SearchPanel from "../search-panel/search-panel";
+import AppFilter from "../app-filter/app-filter";
+import EmployeesList from "../employees-list/employees-list";
+import EmployeesAddForm from "../employees-add-form/employees-add-form";
+
+import "./app.css";
+
+function App() {
+  const data = [
+    {
+      name: "Alex",
+      salary: 800,
+      increase: true,
+    },
+    {
+      name: "John",
+      salary: 250,
+      increase: true,
+    },
+    {
+      name: "Tom",
+      salary: 1100,
+      increase: false,
+    },
+  ];
+  return (
+    <div className="app">
+      <AppInfo />
+      <div className="search-panel">
+        <SearchPanel />
+        <AppFilter />
+      </div>
+      <EmployeesList data={data} />
+      <EmployeesAddForm />
+    </div>
+  );
+}
+
+export default App;
