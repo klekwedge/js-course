5c4d16a38a36c3cddc4cdcf519ac79ced87e541a
klekkwedge <antonov.andrey.10101@gmail.com>
2022-05-09 15:34:29 +0300

    feat: add 25 lesson

---
+++ 4. React library. A basic level of/25 Styles in React. Styled Components/my-app/src/App.js
@@ -0,0 +1,93 @@
+import { Component } from "react";
+import styled from "styled-components";
+import "./App.css";
+
+const EmpItem = styled.div`
+  padding: 20px;
+  margin-bottom: 15px;
+  border-radius: 5px;
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
+  a {
+    display: block;
+    margin: 10px 0 10px 0;
+    color: ${(props) => (props.active ? "orange" : "black")};
+  }
+
+  input {
+    display: block;
+    margin-top: 10px;
+  }
+`;
+
+const Header = styled.h2`
+  font-size: 22px;
+`;
+
+export const Button = styled.button`
+  display: block;
+  padding: 5px 15px;
+  background-color: gold;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
+`;
+
+class WhoAmI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      years: 27,
+      position: "",
+    };
+  }
+
+  nextYear = () => {
+    this.setState((state) => ({
+      years: state.years + 1,
+    }));
+  };
+
+  commitInputChanges = (event, color) => {
+    console.log(color);
+    this.setState({
+      position: event.target.value,
+    });
+  };
+
+  render() {
+    const { name, surname, link } = this.props;
+    const { position, years } = this.state;
+    return (
+      <EmpItem active>
+        <Button onClick={this.nextYear}>+++</Button>
+        <Header>
+          My name is {name}, surname - {surname}, age - {years}, position -{" "}
+          {position}
+        </Header>
+        <a href={link}>My profile</a>
+        <form>
+          <span>Введите должность:</span>
+          <input
+            type="text"
+            onChange={(event) => this.commitInputChanges(event, "some color")}
+          ></input>
+        </form>
+      </EmpItem>
+    );
+  }
+}
+
+const Wrapper = styled.div`
+  width: 600px;
+  margin: 80px auto 0 auto;
+`;
+
+function App() {
+  return (
+    <Wrapper>
+      <WhoAmI name="John" surname="Smith" link="google.com" />
+      <WhoAmI name="Tom" surname="Hanks" link="vk.com" />
+    </Wrapper>
+  );
+}
+
+export default App;
