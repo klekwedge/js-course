5980f04ff50f1729af1bd14b27d17643d3ba1d59
klekkwedge <antonov.andrey.10101@gmail.com>
2022-05-09 14:06:51 +0300

    feat: add 23 lesson

---
+++ 4. React library. A basic level of/23 Styles in React. CSS and SASS SCSS/my-app/src/components/employees-add-form/employees-add-form.js
@@ -0,0 +1,67 @@
+import { Component } from "react";
+// import "./employees-add-form.css";
+
+import "./employees-add-form.scss";
+
+class EmployeesAddForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      salary: "",
+    };
+  }
+
+  onValueChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    let { name, salary } = this.state;
+    if (name.length > 3 && salary) {
+      this.props.onAdd(name, salary);
+    }
+
+    this.setState({
+      name: "",
+      salary: "",
+    });
+  };
+
+  render() {
+    const { name, salary } = this.state;
+    return (
+      <div className="app-add-form">
+        <h3>Добавьте нового сотрудника</h3>
+        <form className="add-form d-flex" onSubmit ={this.onSubmit}>
+          <input
+            type="text"
+            className="form-control new-post-label"
+            name="name"
+            value={name}
+            placeholder="Как его зовут?"
+            onChange={this.onValueChange}
+          />
+          <input
+            type="number"
+            className="form-control new-post-label"
+            name="salary"
+            value={salary}
+            placeholder="З/П в $?"
+            onChange={this.onValueChange}
+          />
+
+          <button type="submit" className="btn btn-outline-light">
+            Добавить
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default EmployeesAddForm;
