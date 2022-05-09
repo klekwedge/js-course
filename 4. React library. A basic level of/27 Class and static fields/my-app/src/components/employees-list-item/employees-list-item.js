5166715e11250bc578ee1220386775bfed4bfebf
klekkwedge <antonov.andrey.10101@gmail.com>
2022-05-09 16:46:16 +0300

    feat: add 27 lesson

---
+++ 4. React library. A basic level of/27 Class and static fields/my-app/src/components/employees-list-item/employees-list-item.js
@@ -0,0 +1,55 @@
+import "./employees-list-item.css";
+
+const EmployeesListItem = (props) => {
+  const {
+    name,
+    salary,
+    onDelete,
+    onToggleProp,
+    increase,
+    rise,
+  } = props;
+
+  let classNames = "list-group-item d-flex justify-content-between";
+  if (increase) {
+    classNames += " increase";
+  }
+
+  if (rise) {
+    classNames += " like";
+  }
+
+  return (
+    <li className={classNames}>
+      <span
+        onClick={onToggleProp}
+        className="list-group-item-label"
+        data-toggle="rise"
+      >
+        {name}
+      </span>
+      <input
+        type="text"
+        className="list-group-item-input"
+        defaultValue={salary + "$"}
+      />
+      <div className="d-flex justify-content-center align-items-center">
+        <button
+          onClick={onToggleProp}
+          data-toggle="increase"
+          type="button"
+          className="btn-cookie btn-sm "
+        >
+          <i className="fas fa-cookie"></i>
+        </button>
+
+        <button onClick={onDelete} type="button" className="btn-trash btn-sm ">
+          <i className="fas fa-trash"></i>
+        </button>
+        <i className="fas fa-star"></i>
+      </div>
+    </li>
+  );
+};
+
+export default EmployeesListItem;
